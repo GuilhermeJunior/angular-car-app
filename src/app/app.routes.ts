85a3248eeaa1f579bc05948a768fe628c5d5e43b
Guilherme Chamorro <poltegaisnt@gmail.com>
2025-11-06 16:49:08 -0300

    adicionando template login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { CarroListComponent } from './components/carro/carro-list/carro-list.component';
 import { CarrodetailsComponent } from './components/carro/carrodetails/carrodetails.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "carros", pathMatch: 'full' },
-  { path: "carros", component: CarroListComponent },
-  { path: "carros/:id", component: CarrodetailsComponent },
-  { path: "carros/:id/edit", component: CarrodetailsComponent },
-  { path: "carros/new", component: CarrodetailsComponent }
+  { path: "", redirectTo: "login", pathMatch: 'full' },
+  { path: "login", component: LoginComponent },
+  { path: "admin", component: PrincipalComponent , children: [
+      { path: "carros", component: CarroListComponent },
+      { path: "carros/:id", component: CarrodetailsComponent },
+      { path: "carros/:id/edit", component: CarrodetailsComponent },
+      { path: "carros/new", component: CarrodetailsComponent }
+  ]}
 ];
