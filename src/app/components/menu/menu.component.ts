d5d084e9b0d588d52209699c1c04aa646adb1ef7
Guilherme Chamorro <poltegaisnt@gmail.com>
2025-11-13 08:44:05 -0300

    seguranca

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+  loginService = inject(LoginService);
+
+  isUserAdmin() {
+    return this.loginService.hasRole("ROLE_ADMIN");
+  }
 
 }
